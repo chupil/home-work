22a4e43ac555fe60410fccf7a0ffbf344c19cecc
Yura <chupil87@gmail.com>
2017-12-12 09:00:29 +0000

    Initial

---
+++ js/formula.js
@@ -0,0 +1,3 @@
+const aNum = Number(prompt('Впиши цифрове значення \"a\"'));
+const bNum = Number(prompt('Впиши цифрове значення \"b\"')));
+const cNum = Number(prompt('Впиши цифрове значення \"c\"')));
