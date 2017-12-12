408533c83963b78bd89e0584cee2d66920f87843
Yura <chupil87@gmail.com>
2017-12-12 16:42:10 +0000

    v16

--- js/index.js
+++ js/index.js
@@ -1,17 +1,26 @@
 const aNum = Number(prompt('Впиши цифрове значення \"a\"'));
+document.write('Число А' +'='+ aNum);
+document.write('<br>');
 const bNum = Number(prompt('Впиши цифрове значення \"b\"'));
+document.write('Число B' +'='+ bNum);
+document.write('<br>');
 const cNum = Number(prompt('Впиши цифрове значення \"c\"'));
+document.write('Число C' +'='+ cNum);
+document.write('<br>');
 var disc = bNum * bNum - 4 * aNum * cNum;
+document.write('Дискримінант' +'='+ disc);
+document.write('<br>');
 
   if (disc < 0) {  
-      alert(disc);    
-    } else if (disc == 0) {
-         var x = (-bNum) / (2 * aNum);
-         alert(x);
+      var noRes ="без рішення";    
+    } else if (disc === 0) {
+         var x = (-bNum) / (2 * aNum);    
     } else if (disc > 0){
     var x1 = (-bNum + Math.sqrt(disc)) / (2 * aNum);
     var x2 = (-bNum - Math.sqrt(disc)) / (2 * aNum);
-    alert(disc);
-    alert(x1);
-    alert(x2);
    }
+document.write('Не має дійсних коренів' +'='+ noRes);
+document.write('<br>');
+document.write('Єдиний корінь ' +'='+ x);
+document.write('<br>');
+document.write('Корінь x1 ' +'= '+ x1 + '<br>'+ 'Корінь x2' +'= '+ x2);
